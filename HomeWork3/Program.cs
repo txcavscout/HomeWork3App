d42b510fd8a67b9f5db3ad5c92f969310b60a481
Bob D <txcavscout@live.com>
2022-05-11 22:44:10 -0700

    Add project files.

---
+++ HomeWork3/Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+string? firstName = null;
+
+Console.Write("Enter your first name: ");
+firstName = Console.ReadLine();
+
+if (firstName.ToLower() == "tim" || firstName.ToLower() == "timothy")
+{
+    Console.WriteLine("Hello Professor.");
+}
+else if (String.IsNullOrEmpty(firstName))
+{
+    Console.WriteLine("You did not enter a name fool.");
+}
+else
+{
+    Console.WriteLine("Greetings fellow student.");
+}
+
